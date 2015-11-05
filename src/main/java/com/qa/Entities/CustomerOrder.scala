cc3ddaa08dc06e9a2d5ecca36b7a44313292ab74
dgordon <dgordon>
2015-11-05 14:42:35 +0000

    First Push to WOTScala

--- src/main/java/com/qa/Entities/CustomerOrder.scala
+++ src/main/java/com/qa/Entities/CustomerOrder.scala
@@ -3,6 +3,23 @@ package com.qa.Entities
 /**
  * @author dgordon
  */
-class CustomerOrder {
+class CustomerOrder (customerOrderID:Int, customerID:Int, customerOrderDate:String, var customerOrderStatus:String, customerOrderTotal:String){
   
+   def getCustomerOrderID() :Int = {
+    customerOrderID
+  }
+  
+  def getCustomerID() :Int = {
+    customerID
+  }
+   
+  def getCustomerOrderDate() :String = {
+    customerOrderDate
+  }
+    
+  def getCustomerOrderTotal() :String = {
+    customerOrderTotal 
+   }
+
+
 }
--- src/main/java/com/qa/Entities/PurchaseOrderLine.scala
+++ src/main/java/com/qa/Entities/PurchaseOrderLine.scala
@@ -3,6 +3,16 @@ package com.qa.Entities
 /**
  * @author dgordon
  */
-class PurchaseOrderLine {
+class PurchaseOrderLine (purchaseOrderLineID:Int,productID:Int,var quantity:Int){
+
+  def getPurchaseOrderLineID() :Int = {
+    purchaseOrderLineID
+  }
+   
+  def getProductID :Int = {
+    productID
+  }
+    
+
   
 }
