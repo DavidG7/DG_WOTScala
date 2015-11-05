abe487246bd36b9f4814d4526d9afcc09b85accf
dgordon <dgordon>
2015-11-05 12:22:58 +0000

    First Push to WOTScala

--- src/main/java/com/qa/Data/DummyData.scala
+++ src/main/java/com/qa/Data/DummyData.scala
@@ -1,6 +1,7 @@
 package com.qa.Data
 import com.qa.Entities.Employee
 import com.mongodb.casbah.Imports._
+import com.qa.MongoData.MongoDataTest
 
 /**
  * @author dgordon
@@ -8,9 +9,24 @@ import com.mongodb.casbah.Imports._
 object DummyData {
   
   def main(args: Array[String]){
-    //val mongoClient = MongoClient("localhost", 27017)
-    val employee = new Employee(1, "Jim Styne", "js7" , "I like gnonmes!", 1)
-    println(employee.getEmployeeName());
+ 
+   /* val mongoClient = MongoClient("localhost", 27017)
+    val db = mongoClient("WOTS")
+    val coll = db("Employee")
+    val temp:Array[Employee] = new Array[Employee](3)
+    val allDocs = coll.find()
+    var count:Int = 0
+    println( allDocs )
+     for(doc <- allDocs) {
+       count+.1
+       //val employee = new Employee((doc.get("_id"),doc.get("employeeName"),doc.get("employeeUserName"),doc.get("employeePassword"),doc.get("accessLevl"))
+       val employee = new Employee(Integer.parseInt(doc.get("_id").toString().substring(0,1)),doc.get("employeeName").toString(),doc.get("employeeUserName").toString(),
+           doc.get("employeePassword").toString(),Integer.parseInt(doc.get("accessLevel").toString().substring(0,1)))
+       temp(count) = employee
+     }
+     println(temp(0).getEmployeeName());*/
     
+     val mongo = new MongoDataTest
+     println(mongo.employeeMongoData()(0).getEmployeeName())
   }
 }
---
+++ src/main/java/com/qa/MongoData/MongoDataTest.scala
@@ -0,0 +1,30 @@
+package com.qa.MongoData
+
+import com.mongodb.casbah.MongoClient
+import com.qa.Entities.Employee
+
+
+
+/**
+ * @author dgordon
+ */
+class MongoDataTest {
+  val mongoClient = MongoClient("localhost", 27017)
+  val db = mongoClient("WOTS")
+  
+    def employeeMongoData() = Array[Employee]  {
+    val coll = db("Employee")
+    val temp:Array[Employee] = new Array[Employee](coll.count())
+    val allDocs = coll.find()
+    var count:Int = 0
+    println( allDocs )
+     for(doc <- allDocs) {
+       count+.1
+       val employee = new Employee(Integer.parseInt(doc.get("_id").toString().substring(0,1)),doc.get("employeeName").toString(),doc.get("employeeUserName").toString(),
+           doc.get("employeePassword").toString(),Integer.parseInt(doc.get("accessLevel").toString().substring(0,1)))
+       temp(count) = employee
+     }
+     temp(0)
+
+  }
+}
